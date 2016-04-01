71dd5174c8ddc61a1ac252dd95cd87a2b0e7ba1b
alfredodi <adiaz118@student.rcc.edu>
2016-04-01 08:15:40 -0700

    Recursive Exponentiation and GCD Methods
    Provide a recursive method using the Exponentiation By Squaring method
    named integerPower( base, exponent ) that returns the value of
    base^exponent and a recursive method named gcd( number1, number2 ) that
    returns the greatest common divisor of the numbers, number1 and number2.

---
+++ Hmwk/Assignment 5/RecursiveExponentiationandGCD/src/recursiveexponentiationandgcd/RecursiveExponentiationandGCD.java
@@ -0,0 +1,13 @@
+package recursiveexponentiationandgcd;
+
+// Import Libraries
+
+public class RecursiveExponentiationandGCD {
+
+    public static void main(String[] args) {
+        // Main code
+    }
+    // Methods
+}
+
+    
